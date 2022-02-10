d54eb05cfbd938561c93345b7bcb51f2cc175f3f
Yokey <zyf6@outlook.com>
2022-02-10 21:59:09 +0800

    完成数据加载部分

--- nnunet/training/data_augmentation/level_set.py
+++ nnunet/training/data_augmentation/level_set.py
@@ -1,41 +1,53 @@
 import numpy as np
 from scipy.ndimage import distance_transform_edt as distance
 from skimage import segmentation as skimage_seg
+from batchgenerators.transforms.abstract_transforms import AbstractTransform
 
 
-def compute_level_set(img_gt, out_shape):
-    """
-    compute the signed distance map of binary mask
-    input: segmentation, shape = (batch_size, x, y, z)
-    output: the Signed Distance Map (SDM)
-    sdf(x) = 0; x in segmentation boundary
-             -inf|x-y|; x in segmentation
-             +inf|x-y|; x out of segmentation
-    normalize sdf to [-1,1]
-    """
+class LevelSetTransform(AbstractTransform):
+    def __init__(self, input_key="target", output_key="target"):
+        self.input_key = input_key
+        self.output_key = output_key
 
-    img_gt = img_gt.astype(np.uint8)
-    normalized_sdf = np.zeros(out_shape)
+    def __call__(self, **data_dict):
+        """
+        compute the signed distance map of binary mask
+        input: segmentation, shape = (batch_size, x, y, z)
+        output: the Signed Distance Map (SDM)
+        sdf(x) = 0; x in segmentation boundary
+                 -inf|x-y|; x in segmentation
+                 +inf|x-y|; x out of segmentation
+        normalize sdf to [-1,1]
+        """
+        img_gt = data_dict[self.input_key][0]
+        img_gt = img_gt.astype(np.uint8)
+        out_shape = img_gt.shape
+        normalized_sdf = np.zeros(out_shape)
 
-    for b in range(out_shape[0]):  # batch size
-        posmask = img_gt[b].astype(bool)
-        if posmask.any():
-            negmask = ~posmask
-            posdis = distance(posmask)
-            negdis = distance(negmask)
-            boundary = skimage_seg.find_boundaries(posmask, mode='inner').astype(np.uint8)
-            sdf = (negdis - np.min(negdis)) / (np.max(negdis) - np.min(negdis)) - (posdis - np.min(posdis)) / (
-                    np.max(posdis) - np.min(posdis))
-            sdf[boundary == 1] = 0
-            normalized_sdf[b] = sdf
-            # assert np.min(sdf) == -1.0, print(np.min(posdis), np.max(posdis), np.min(negdis), np.max(negdis))
-            # assert np.max(sdf) ==  1.0, print(np.min(posdis), np.min(negdis), np.max(posdis), np.max(negdis))
+        for b in range(out_shape[0]):  # batch size
+            posmask = img_gt[b].astype(bool)
+            if posmask.any():
+                negmask = ~posmask
+                posdis = distance(posmask)
+                negdis = distance(negmask)
+                boundary = skimage_seg.find_boundaries(posmask, mode='inner').astype(np.uint8)
+                sdf = (negdis - np.min(negdis)) / (np.max(negdis) - np.min(negdis)) - (posdis - np.min(posdis)) / (
+                        np.max(posdis) - np.min(posdis))
+                sdf[boundary == 1] = 0
+                normalized_sdf[b] = sdf
+                # assert np.min(sdf) == -1.0, print(np.min(posdis), np.max(posdis), np.min(negdis), np.max(negdis))
+                # assert np.max(sdf) ==  1.0, print(np.min(posdis), np.min(negdis), np.max(posdis), np.max(negdis))
 
-    return normalized_sdf
+        data_dict[self.output_key] = (normalized_sdf, data_dict[self.input_key])
+        return data_dict
 
 
-if __name__ == '__main__':
-    np.random.seed(123)
-    image_gt = np.random.randint(0, 1, size=(3, 4, 4, 4))
-    res = compute_level_set(image_gt, image_gt.shape)
-    print(res.shape)
+# if __name__ == '__main__':
+#     from batchviewer import view_batch
+#
+#     np.random.seed(123)
+#     image_gt = np.random.randint(0, 2, size=(3, 30, 300, 300))
+#     res = compute_level_set(image_gt, image_gt.shape)
+#     view_batch(image_gt, width=300, height=300)
+#     view_batch(res, width=300, height=300)
+#     print(res.shape)
---
+++ nnunet/training/network_training/nnUNetTrainerV2_DTC.py
@@ -0,0 +1,123 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+import numpy as np
+from batchgenerators.utilities.file_and_folder_operations import *
+from torch import nn
+
+from nnUNetTrainerV2 import nnUNetTrainerV2
+from nnunet.network_architecture.neural_network import SegmentationNetwork
+from nnunet.training.data_augmentation.data_augmentation_moreDA import get_moreDA_augmentation
+from nnunet.training.data_augmentation.level_set import LevelSetTransform
+from nnunet.training.dataloading.dataset_loading import unpack_dataset
+from nnunet.training.loss_functions.deep_supervision_DTC import DTCLoss_DTC
+
+
+class nnUNetTrainerV2DTC(nnUNetTrainerV2):
+    """
+    Info for Fabian: same as internal nnUNetTrainerV2_2
+    """
+
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+
+    def initialize(self, training=True, force_load_plans=False):
+        """
+        - replaced get_default_augmentation with get_moreDA_augmentation
+        - enforce to only run this code once
+        - loss function wrapper for deep supervision
+
+        :param training:
+        :param force_load_plans:
+        :return:
+        """
+        if not self.was_initialized:
+            maybe_mkdir_p(self.output_folder)
+
+            if force_load_plans or (self.plans is None):
+                self.load_plans_file()
+
+            self.process_plans(self.plans)
+
+            self.setup_DA_params()
+
+            ################# Here we wrap the loss for deep supervision ############
+            # we need to know the number of outputs of the network
+            net_numpool = len(self.net_num_pool_op_kernel_sizes)
+
+            # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+            # this gives higher resolution outputs more weight in the loss
+            weights = np.array([1 / (2 ** i) for i in range(net_numpool)])
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            mask = np.array([True] + [True if i < net_numpool - 1 else False for i in range(1, net_numpool)])
+            weights[~mask] = 0
+            weights = weights / weights.sum()
+            self.ds_loss_weights = weights
+            # now wrap the loss
+            self.loss = DTCLoss_DTC(self.loss, self.ds_loss_weights)
+            ################# END ###################
+
+            self.folder_with_preprocessed_data = join(self.dataset_directory, self.plans['data_identifier'] +
+                                                      "_stage%d" % self.stage)
+            if training:
+                self.dl_tr, self.dl_val = self.get_basic_generators()
+                if self.unpack_data:
+                    print("unpacking dataset")
+                    unpack_dataset(self.folder_with_preprocessed_data)
+                    print("done")
+                else:
+                    print(
+                        "INFO: Not unpacking data! Training may be slow due to that. Pray you are not using 2d or you "
+                        "will wait all winter for your model to finish!")
+
+                self.tr_gen, self.val_gen = get_moreDA_augmentation(
+                    self.dl_tr, self.dl_val,
+                    self.data_aug_params[
+                        'patch_size_for_spatialtransform'],
+                    self.data_aug_params,
+                    deep_supervision_scales=self.deep_supervision_scales,
+                    pin_memory=self.pin_memory,
+                    use_nondetMultiThreadedAugmenter=False,
+                    level_set_func=LevelSetTransform()
+                )
+                self.print_to_log_file("TRAINING KEYS:\n %s" % (str(self.dataset_tr.keys())),
+                                       also_print_to_console=False)
+                self.print_to_log_file("VALIDATION KEYS:\n %s" % (str(self.dataset_val.keys())),
+                                       also_print_to_console=False)
+            else:
+                pass
+
+            self.initialize_network()
+            self.initialize_optimizer_and_scheduler()
+
+            assert isinstance(self.network, (SegmentationNetwork, nn.DataParallel))
+        else:
+            self.print_to_log_file('self.was_initialized is True, not running self.initialize again')
+        self.was_initialized = True
+
+    def run_online_evaluation(self, output, target):
+        """
+        due to deep supervision the return value and the reference are now lists of tensors. We only need the full
+        resolution output because this is what we are interested in in the end. The others are ignored
+        :param output:
+        :param target:
+        :return:
+        """
+        target = target[1][0]
+        output = output[1][0]
+        return super().run_online_evaluation(output, target)
