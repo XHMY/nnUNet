2c824aafea41c2ac99ce76e68407b4a428a2e6d6
Yokey <zyf6@outlook.com>
2022-03-14 12:39:22 +0800

    Update nnUNetTrainerV2_TransferLess.py

--- nnunet/training/network_training/nnUNetTrainerV2_TransferLess.py
+++ nnunet/training/network_training/nnUNetTrainerV2_TransferLess.py
@@ -1,7 +1,7 @@
-from nnunet.training.network_training import nnUNetTrainerV2
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
 
 
-class nnUNetTrainerV2_TransferLess(nnUNetTrainerV2):
+class nnUNetTrainerV2TransferLess(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
