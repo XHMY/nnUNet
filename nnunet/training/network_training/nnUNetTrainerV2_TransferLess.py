af90516953ab29f4c96b2ac0d0701e2c091afa8d
Yokey <zyf6@outlook.com>
2022-03-15 09:18:30 +0800

    Update nnUNetTrainerV2_TransferLess.py

--- nnunet/training/network_training/nnUNetTrainerV2_TransferLess.py
+++ nnunet/training/network_training/nnUNetTrainerV2_TransferLess.py
@@ -1,7 +1,17 @@
+from nnunet.training.network_training.nnUNetTrainer import nnUNetTrainer
 from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
 
 
 class nnUNetTrainerV2TransferLess(nnUNetTrainerV2):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.max_num_epochs = 50
+        self.initial_lr = 1e-4
+        self.num_batches_per_epoch = 50
+
+class nnUNetTrainerTransferLess(nnUNetTrainer):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
